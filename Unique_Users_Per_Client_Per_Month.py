e1a971bb56aee22f058ac2ca9b82b79baa53ac45
SwarupNanche <140883044+SwarupNanche@users.noreply.github.com>
2026-07-16 14:37:08 +0530

    Update Unique_Users_Per_Client_Per_Month.py

--- Unique_Users_Per_Client_Per_Month.py
+++ Unique_Users_Per_Client_Per_Month.py
@@ -1,6 +1,6 @@
--- ID 2024
--- Write a query that returns the number of unique users per client for each month. 
--- Assume all events occur within the same year, so only month needs to be be in the output as a number from 1 to 12
+# -- ID 2024
+# -- Write a query that returns the number of unique users per client for each month. 
+# -- Assume all events occur within the same year, so only month needs to be be in the output as a number from 1 to 12
 
 import pandas as pd
 
