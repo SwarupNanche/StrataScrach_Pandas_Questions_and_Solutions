27b012156200a8b67ffa8fabaeb45adf874ccac4
SwarupNanche <140883044+SwarupNanche@users.noreply.github.com>
2026-07-16 14:36:50 +0530

    Update and rename Unique_Users_Per_Client_Per_Month.sql to Unique_Users_Per_Client_Per_Month.py

--- Unique_Users_Per_Client_Per_Month.py
+++ Unique_Users_Per_Client_Per_Month.py
@@ -2,7 +2,8 @@
 -- Write a query that returns the number of unique users per client for each month. 
 -- Assume all events occur within the same year, so only month needs to be be in the output as a number from 1 to 12
 
-select client_id,month(time_id),count(distinct user_id)
-from fact_events
-group by client_id,month(time_id)
-order by client_id,month(time_id)
+import pandas as pd
+
+fact_events['month_num']=fact_events['time_id'].dt.month
+final_df=fact_events.groupby(['client_id','month_num'])['user_id'].nunique()
+final_df.head()
